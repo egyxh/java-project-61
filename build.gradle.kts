e7da10022123805f5c42b3f24e6cf6e059c07c13
egor_semichev <thebestists@gmail.com>
2025-04-16 18:39:59 +0300

    4'th part

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("com.github.ben-manes.versions") version "0.51.0"
     application
+    id("checkstyle")
+
 }
 
 application {
@@ -33,4 +35,10 @@ tasks {
 
 tasks.named<JavaExec>("run") {
     standardInput = System.`in`
+}
+
+checkstyle {
+    toolVersion = "10.12.4" 
+    configFile = rootProject.file("config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false 
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,12 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-	public static void greetUser() {
-		Scanner sc = new Scanner(System.in);
-        	System.out.println("Welcome to the Brain Games!");
-        	System.out.print("May I have your name? ");
-        	String userName = sc.nextLine();
-        	System.out.println("Hello, " + userName + "!");
-        	sc.close();
-	}
+    public static void greetUser() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String userName = sc.nextLine();
+        System.out.println("Hello, " + userName + "!");
+        sc.close();
+    }
 }
